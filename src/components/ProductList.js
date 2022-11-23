38e00d7a73cd94c2a1f49f79c9d85c048e4cf378
nusrat jahan <nusrat.jahan@dsinnovators.com>
2022-11-23 18:26:56 +0600

    axios added

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,26 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Title from "./Title";
+import Product from "./Product";
 
 
-export default class ProductList extends Component{
-    render(){
-        return(
-            <React.Fragment>
-                 <div className="py-5">
-                    <div className="container">
-                    <Title name="our" title="products"/>
-                       <div className="grid grid-cols-3">
-                         {/* <ProductConsumer>
-                             {value=>{
-                               return value.products.map(product =>{
-                                   return <Product key={product.id} product={product} />;
-                               });
-                             }}
-                         </ProductConsumer> */}
-                       </div>
-                       </div> 
-                    </div>
-            </React.Fragment> 
-        )
-    }
+export default class ProductList extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div className="py-5">
+          <div className="container">
+            <Title name="our" title="products" />
+          </div>
+        </div>
+        <section>
+          <Product />
+        </section>
+      </React.Fragment>
+    );
+  }
 }
